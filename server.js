9681d67154a7cdab27fbddffaa620283dc31aa33
ArijitPatra2906 <patraarijit440@gmail.com>
2026-02-17 16:07:42 +0530

    add TLS support for MongoDB connection

--- server.js
+++ server.js
@@ -114,6 +114,8 @@ server.use(
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  tls: true,
+  tlsAllowInvalidCertificates: false,
 })
 const db = mongoose.connection
 db.on('error', (err) => {
